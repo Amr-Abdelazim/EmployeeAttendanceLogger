0460562b0e1abbe5668e09dd51ab7fe41dc20bd5
Amr Abdelazim <amrabdelazim100@gmail.com>
2024-09-15 06:10:50 +0300

    Initial commit

---
+++ Employee.py
@@ -0,0 +1,87 @@
+from datetime import datetime, timedelta
+class Employee:
+    def __init__(self,id,name,department):
+        self.id = id
+        self.name = name
+        self.department = department
+        self.logs = dict()
+    
+    def add_log(self,date_str):
+        date_obj = datetime.strptime(date_str, "%m/%d/%Y %I:%M:%S %p")
+        date_only = date_obj.date()
+        time_only = date_obj.time()
+        if date_only in self.logs:
+            if time_only <= self.logs[date_only][0]:
+                self.logs[date_only][0] = time_only
+            elif time_only >= self.logs[date_only][1]:
+                self.logs[date_only][1] = time_only
+        else:
+            self.logs[date_only] = [time_only, time_only]
+
+    def format_12h(self,time_only):
+        # Create a new datetime object with a dummy date and the extracted time
+        time_as_datetime = datetime.combine(datetime.min, time_only)
+
+        # Format the time in 12-hour format with AM/PM
+        time_12h = time_as_datetime.strftime("%I:%M %p")
+
+        return time_12h
+
+    def time_handle(self,time_log):# time_log = [first_time, last_time]
+        if len(time_log) == 0:
+            return ["NoFingerPrint", "NoFingerPrint", "NoFingerPrint"]
+        dummy_date = datetime(2024, 1, 1)  # Any arbitrary date
+        time0 = datetime.combine(dummy_date, time_log[0])
+        time1 = datetime.combine(dummy_date, time_log[1])
+        diff = time1 - time0
+        hours = int(diff.total_seconds()//3600)
+        minutes = int((diff.total_seconds() % 3600) // 60)
+        
+        check_in = self.format_12h(time_log[0])
+        check_out = self.format_12h(time_log[1])
+
+        total_work = f"{hours}:{minutes}"
+
+        if hours <= 1:# if employee worked more than 1 hour then no missing in (check_in, check_out)
+            if time_log[0].hour >= 12:# if its pm so missing check_in
+                check_in = "Missing"
+                total_work = "Missing check_in"
+            else:
+                check_out = "Missing"
+                total_work = "Missing check_out"
+        
+        
+        return check_in, check_out, total_work
+
+
+
+    def add_empty_days(self,first, last):# loop over all dayes form first to last and add not exists days
+        date1 = datetime.strptime(first, "%m/%d/%Y")
+        date2 = datetime.strptime(last, "%m/%d/%Y")
+        while date1 <= date2:
+            if date1.date() not in self.logs:
+                self.logs[date1.date()] = []
+            date1 += timedelta(days=1)
+
+
+    def fetch_data(self, exceptions_dayes = [], exceptions_dates = []):
+        data = []
+        for key in sorted(self.logs):
+            if key.strftime("%m/%d/%Y") in exceptions_dates:
+                continue
+            if key.strftime("%A") in exceptions_dayes:
+                continue
+            value = self.logs[key]
+            check_in, check_out, worked_hours = self.time_handle(value)
+            data.append([self.id, self.name , self.department, key.strftime("%m/%d/%Y"), check_in, check_out, worked_hours])
+
+        return data
+
+
+
+
+
+        
+
+
+
---
+++ main.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from Employee import Employee
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Font
+import os
+
+employees = dict()
+
+
+
+file_path = input("Enter File Path :") 
+first_day = input("Enter First Day in formate 'm/d/y':")
+last_day = input("Enter Last Day in formate 'm/d/y':")
+
+# dayes : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+exceptions_dayes=['Friday', 'Saturday']
+exceptions_dates=[]
+
+#choice = input("Enter more details? [Y/N] :")
+#if choice.lower() == "y":
+#    print("1- Enter exceptions_days.")
+#    print("2- Enter exceptions_dates.")
+#    while True:
+#        choice = input("Enter your choice [1 or 2] :")
+#        if choice in ['1','2']:
+#            break
+#    if choice == '1':
+        
+
+
+
+df = pd.read_excel(file_path, sheet_name = 0, engine='xlrd')
+    
+count = 0
+
+for index, row in df.iterrows():
+    row = row.to_dict()
+    id = row['No.']
+    if id not in employees:
+        employees[id] = Employee(id=id, name=row['Name'],department=row['Department'])
+    employees[id].add_log(date_str=row['Date/Time'])
+
+for key in sorted(employees):
+    employees[key].add_empty_days(first_day,last_day)
+
+
+wb = Workbook()
+ws = wb.active
+
+column_names = ["No.", "Name", "Department", "date", "check_in", "check_out", "worked_hours"]
+ws.append(column_names)
+
+fill = PatternFill(start_color="000000", end_color="000000", fill_type="solid")
+font = Font(color="FFFFFF")
+
+columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+for i in range(0,len(columns)):
+    ws.column_dimensions[columns[i]].width = 25
+
+total_work = 0
+
+for key in sorted(employees):
+    emp_data = employees[key].fetch_data(exceptions_dayes = exceptions_dayes,exceptions_dates = exceptions_dates)
+    total_work = 0
+    for row in emp_data:
+        ws.append(row)
+        worked_time = row[-1].split(':')
+        if len(worked_time) > 1:
+            total_work += int(worked_time[0]) * 60 + int(worked_time[1])
+    
+    ws.append(['','','','','','',f"{total_work // 60}:{total_work % 60}"]) # empty row
+    for cell in ws[ws.max_row]:
+        cell.fill = fill
+        cell.font = font
+
+
+save_path = "output"
+i = 1
+while True:
+    if not os.path.isfile(save_path + str(i) + '.xlsx'):
+        break
+    i += 1
+
+wb.save(save_path + str(i) + '.xlsx')
+
+
